e76c2bbf44d3df18a97346f910cd399ba3f3c67a
Ian Hoegen <ianhoegen@gmail.com>
2018-08-30 10:40:49 -0700

    Add snapshot rotation

--- pkg/cloudproviders/aws/aws.go
+++ pkg/cloudproviders/aws/aws.go
@@ -11,6 +11,10 @@ import (
 //CreateBackup finds ETCD volumes, then creates snapshots
 func CreateBackup() ([]string, error) {
 	ec2Client := ec2.New(session.New())
+	err := RotateSnapshots(ec2Client, 4000)
+	if err != nil {
+		fmt.Println(err)
+	}
 	volumes, err := FindETCDVolumes(ec2Client)
 	if err != nil {
 		return nil, err
@@ -59,4 +63,34 @@ func CreateSnapshots(client *ec2.EC2, volumes []ec2.Volume) ([]string, error) {
 		return snapshots, nil
 	}
 	return snapshots, fmt.Errorf(errors)
+}
+
+// RotateSnapshots ensures that no more than depth amount of snapshots exist
+func RotateSnapshots(client *ec2.EC2, depth int) error {
+	snapshots, err := client.DescribeSnapshots(&ec2.DescribeSnapshotsInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name: aws.String("tag-key"),
+				Values: []*string{
+					aws.String("k8s.io/etcd/events"),
+					aws.String("k8s.io/etcd/main"),
+				},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	if len(snapshots.Snapshots) <= depth {
+		return nil
+	}
+	for i := len(snapshots.Snapshots) - 1; i >= depth; i-- {
+		_, err := client.DeleteSnapshot(&ec2.DeleteSnapshotInput{
+			SnapshotId: snapshots.Snapshots[i].SnapshotId,
+		})
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	return nil
 }
